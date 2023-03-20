6ff41bdc67a97e54e886b4adc261d44637c6d7df
jnguyen191 <jenny.nguyentnt@gmail.com>
2023-03-19 23:47:11 -0400

    update

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "73c922cb973ec016b0db3b18d1c1ea61"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "21f4509cff2248d08c0daf0145006453"
